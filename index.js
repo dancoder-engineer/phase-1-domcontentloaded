e75b138b41411cc4ec560752a1639601d6fd0f2f
Dan Seminara <japaneseforgaming@gmail.com>
2021-08-20 16:19:21 -0400

    commit tree a73ff9fac3b76272ba90fd153757726df47f31bc

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Your code goes here
+function textChange() {
+    document.getElementById(`text`).innerText = `This is really cool!`
+
+}
+
+document.addEventListener('DOMContentLoaded', () => { document.querySelector('p').textContent = `This is really cool!`; } );
+//document.addEventListener('DOMContentLoaded', textChange);
+
+//document.addEventListener('DOMContentLoaded', () => { document.getElementById(`text`).innerText = `This is really cool!`; } );
